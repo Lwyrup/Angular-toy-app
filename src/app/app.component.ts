0674af39fabaa45d372b8881f1042a7ff81d986e
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-20 13:09:35 -0500

    Added initilization logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from './hero';	
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
+	providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+	constructor(private heroService: HeroService) { }
+
 	title = 'Tour of Heroes';
-	heroes = Hero[];
+	heroes: Hero[];
 	selectedHero: Hero;
 
 	onSelect(hero: Hero): void{
 		this.selectedHero = hero;
+	}
+
+	getHeroes(): void {
+		this.heroes = this.heroService.getHeroes();
+	}
+
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 }
 
