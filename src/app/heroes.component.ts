254da834acc46ef59711b0991a86dccfb67d13e8
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-20 17:22:52 -0500

    Removed providers

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -6,8 +6,7 @@ import { OnInit } from '@angular/core';
 @Component({
 	selector: 'my-heroes',
 	templateUrl: './heroes.component.html',
-	styleUrls: ['./heroes.component.css'],
-	providers: [HeroService]
+	styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit{
