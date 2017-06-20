b5b216e9403335e4b40707ea01860bfc9f3db0fa
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-20 17:07:29 -0500

    Indentation

--- src/app/dashboarrd.component.ts
+++ src/app/dashboarrd.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+	selector: 'my-dashboard',
+	template: '<h3>My Dashboard</h3>'
 })
 
 export class DashboardComponent { }
