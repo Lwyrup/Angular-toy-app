16ec09ef0d8745e4babd8db9f957d74cae28f1a9
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-20 12:08:54 -0500

    Added a select method and selectedhero property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,17 +19,18 @@ const HEROES: Hero[] = [
 ];
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'Tour of Heroes';
-  heroes = HEROES;
-  hero: Hero = {
-  	id: 1,
-  	name: "Windstorm"
-  };
+	title = 'Tour of Heroes';
+	heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero: Hero): void{
+		this.selectedHero = hero;
+	}
 }
 
