82f3e9ec8b6437c38f2d0a48e528228b31516959
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-21 15:20:36 -0500

    Save method implemented

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,7 @@ import { Hero } from './hero';
 export class HeroService {
 
 	private heroesUrl = 'api/heroes';
+	private headers = new Headers({'Content-Type': 'application/json'});
 
 	constructor(private http: Http) { }
 
@@ -20,16 +21,25 @@ export class HeroService {
 			.catch(this.handleError);
 	}
 
-	private handleError(error: any): Promise<any> {
-		console.error('An error occurred', error);
-		return Promise.reject(error.message || error);
-	}
-
 	getHero(id: number): Promise<Hero> {
 		const url = `${this.heroesUrl}/${id}`;
 		return this.http.get(url)
 			.toPromise()
 			.then(response => response.json().data as Hero)
 			.catch(this.handleError);
+	}
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+			.put(url, JSON.stringify(hero), {headers: this.headers})
+			.toPromise()
+			.then(() => hero)
+			.catch(this.handleError);
+	}
+
+	private handleError(error: any): Promise<any> {
+		console.error('An error occurred', error);
+		return Promise.reject(error.message || error);
 	}
 }
